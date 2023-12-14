81c7a436d0ca3bf2b8900ba5b3b71b67ee081d83
Braisarco <braisarco@gmail.com>
2023-12-14 15:42:28 +0100

    devCommit(2)

--- src/main.cs
+++ src/main.cs
@@ -1,5 +1,10 @@
 namespace src;
 
 class main {
-    Proveedor = new Proveedor();
+
+    //Metodo encargado de inicializar a aplicacion, si existen archivos xml, 
+    //collera a info de ahi si non inicializará de 0.
+    public void init(){
+        //TODO inicializar aplicacion
+    }
 }
